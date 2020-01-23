68e0325ce3c3a9371c8f799e40af13c7ed592167
Simon Christ <SimonChrist@gmx.de>
2020-01-23 14:39:19 +0100

    comment comments

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using Test
   err = @test_throws UndefVarError mean
   msg = sprint(Base.showerror, err.value)
   @test msg == "UndefVarError: mean not defined"
-  "Ideally scan stdlibs/ current environment for binding
+  # Ideally scan stdlibs/ current environment for binding
   @test_broken msg == "UndefVarError: mean not defined. Binding mean found in Statistics, load by `using Statistics`."
-  "Alternatively a generic hint about searching for the function and code loading"
+  # Alternatively a generic hint about searching for the function and code loading
   @test_broken msg == "UndefVarError: mean not defined. Search for `mean` on https://docs.julialang.org or https://pkg.julialang.org, then load the package via `using`."
 end
